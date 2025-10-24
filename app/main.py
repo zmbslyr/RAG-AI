7bfc13cd9d70952f7537a2c655a0d43194843068
zmbslyr <mhedgeland@gmail.com>
2025-10-24 16:41:23 -0400

    Slight syntax change in comments

--- app/main.py
+++ app/main.py
@@ -28,17 +28,17 @@ app = FastAPI()
 #.parent removes the file name from the path, returning a path variable to the folder which contains the file.
 BASE_DIR = Path(__file__).resolve().parent
 
-#mounting allows the app to use files found on the disk at the specified location. This allows the HTML to load.
+# mounting allows the app to use files found on the disk at the specified location. This allows the HTML to load.
 app.mount(
     "/static",
     StaticFiles(directory=BASE_DIR / "static"),
     name="static"
 )
 
-#templates is an object that allows jinja to know wherer to read templates from
+# templates is an object that allows jinja to know wherer to read templates from
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-#this loads the web page when accessed through the browser
+# this loads the web page when accessed through the browser
 @app.get("/", response_class=HTMLResponse) #FastAPI expects JSON by default so we must specify HTML
 def serve_index(request: Request): #The request object is created and passed to this function in the background by FastAPI, populated by the HTTP metadata
     return templates.TemplateResponse("index.html", {"request": request})
